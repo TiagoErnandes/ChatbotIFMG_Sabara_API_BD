0197c04b2e1db6252c3880430ac3ec4b5cebc004
paulinnoo <82847474+paulinnoo@users.noreply.github.com>
2021-05-15 14:53:57 -0300

    Update arquivos.js

--- models/arquivos.js
+++ models/arquivos.js
@@ -1,46 +1,38 @@
 const conexao = require("../bancodedados/conexao");
 
 function getAll(res) {
-  const sql = "SELECT * FROM Arquivos;";
-  conexao.query(sql, (erro, resultado) => {
-    if (erro) {
-      res.status(400).json(erro);
-    } else {
-      res.status(200).json(resultado);
-    }
-  });
+    const sql = "SELECT * FROM arquivos;";
+    connection(sql, res);
 }
 
 function add(nome, caminho, res) {
-  const sql = `INSERT INTO Arquivos(nome, caminho, atualizado_em, criado_em, deletado_em) VALUES("${nome}","${caminho}",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP, NULL);`;
-  conexao.query(sql, (erro, resultado) => {
-    if (erro) {
-      res.status(400).json(erro);
-    } else {
-      res.status(200).json(resultado);
-    }
-  });
+    const sql = `INSERT INTO arquivos(nome, caminho, atualizado_em, criado_em, deletado_em) VALUES("${nome}","${caminho}",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP, NULL);`;
+    connection(sql, res);
 }
 
 function erase(id, res) {
-  const sql = `DELETE FROM Arquivos WHERE id=${id}`;
-  conexao.query(sql, (erro, resultado) => {
-    if (erro) {
-      res.status(400).json(erro);
-    } else {
-      res.status(200).json(resultado);
-    }
-  });
+    const sql = `DELETE FROM arquivos WHERE id=${id};`;
+    connection(sql, res);
 }
 
-function edit(id, alt, res) {
-  const sql = `UPDATE Arquivos SET nome='${alt}' WHERE id=${id}`;
-  conexao.query(sql, (erro, resultado) => {
-    if (erro) {
-      res.status(400).json(erro);
-    } else {
-      res.status(200).json(resultado);
-    }
-  });
+function edit(id, nome,caminho, res) {
+    const sql = `UPDATE arquivos SET nome="${nome}" , caminho="${caminho}", atualizado_em = CURRENT_TIMESTAMP WHERE id=${id};`;
+    connection(sql, res);
 }
-module.exports = { getAll, add, erase, edit };
+
+function getOne(id, res) {
+    const sql = `SELECT * FROM arquivos WHERE id=${id};`;
+    connection(sql, res);    
+}
+
+function connection (sql, res){
+    conexao.query(sql, (erro, resultado) => {
+        if (erro) {
+            res.status(400).json(erro);
+        } else {
+            res.status(200).json(resultado);
+        }
+    });
+}
+
+module.exports = { getAll, add, erase, edit, getOne}
