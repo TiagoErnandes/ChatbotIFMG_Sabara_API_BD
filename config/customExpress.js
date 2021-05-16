b003807f0efe6e28b792b0052ed58eeca28a8ee9
Guilherme Luis <21602034@aluno.cotemig.com.br>
2021-05-16 18:59:19 -0300

    express-upload adicionado para a seção de arquivos

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const upload = require("express-fileupload");
 const consign = require("consign");
 const cors = require("cors");
 
@@ -6,6 +7,8 @@ module.exports = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(upload());
+  app.use(express.static("public"));
   consign().include("controllers").into(app);
   return app;
 };
--- controllers/arquivos.js
+++ controllers/arquivos.js
@@ -1,4 +1,5 @@
 const Arquivo = require("../models/arquivos");
+const fs = require("fs");
 
 module.exports = (app) => {
   app.get("/arquivos", (req, res) => {
@@ -16,7 +17,24 @@ module.exports = (app) => {
     Arquivo.add(nome, caminho, res);
   });
 
+  app.post("/arquivos/upload", function (req, res) {
+    console.log(req.files);
+    if (req.files) {
+      console.log(req.files.arquivo);
+      var file = req.files.arquivo;
+      var filename = file.name;
+      file.mv("./public/storage/files/" + filename, function (err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("File Uploaded");
+        }
+      });
+    }
+  });
+
   app.delete("/arquivos/:id", (req, res) => {
+    fs.unlinkSync("./public/" + req.body.caminho);
     const id = parseInt(req.params.id);
     Arquivo.erase(id, res);
   });
@@ -25,7 +43,6 @@ module.exports = (app) => {
     const id = parseInt(req.params.id);
     let nome = req.body.nome;
     let caminho = req.body.caminho;
-    Arquivo.edit(id, nome,caminho, res);
+    Arquivo.edit(id, nome, caminho, res);
   });
-  
 };
