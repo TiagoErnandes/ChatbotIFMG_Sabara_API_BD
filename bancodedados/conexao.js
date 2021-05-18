d3f70f95777d43534f2c676811216e3e0b980b97
tiagoernandes <tiagoernandes@hotmail.com>
2021-05-18 19:21:03 -0300

    conexao com bd

--- bancodedados/conexao.js
+++ bancodedados/conexao.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const conexao = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "mysql743.umbler.com",
+  user: "rootif",
+  password: "chatbot3675",
   database: "chatbot",
 });
 
